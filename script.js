061c46dcc85238f06bd55b8d8f3a91f301b99367
castro077 <castroocampo2304@gmail.com>
2025-02-11 16:32:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// Función para mostrar respuesta
+const mostrarRespuesta = () => {
+  const respuesta = confirm("¿Aceptas pasar San Valentín conmigo? ❤️");
+  if (respuesta) {
+    alert("¡Gracias por no rechazarme. 😊");
+  } else {
+    alert("Bueno,hice lo que pude");
+  }
+};
+
+// Generar corazones
+const heartsContainer = document.querySelector('.hearts');
+for (let i = 0; i < 50; i++) { // Aumentamos el número de corazones
+  const heart = document.createElement('span');
+  heart.style.left = Math.random() * 100 + 'vw';
+  heart.style.animationDuration = Math.random() * 6 + 4 + 's'; // Variamos la duración
+  heart.style.width = `${Math.random() * 30 + 20}px`; // Tamaño aleatorio entre 20px y 50px
+  heart.style.height = heart.style.width; // Mantenemos la proporción
+  heartsContainer.appendChild(heart);
+}
